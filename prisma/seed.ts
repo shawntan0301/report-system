94443fcb425adb9b7a28ca1374b881ba68f31d08
shawntan0301 <shawntan0301@gmail.com>
2025-04-04 01:11:18 +0800

    update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,22 @@
-await prisma.user.createMany({
-  data: [
-    { email: "admin@servihub.com", role: "admin", name: "Admin User" },
-    { email: "user1@servihub.com", name: "User One" },
-  ],
-})
+import { db } from "~/server/db";
 
-await prisma.report.create({
+await db.user.createMany({
+  data: [
+    {
+      email: "admin@servihub.com",
+      role: "admin",
+      name: "Admin User",
+      clerk_id: "123",
+    },
+    { email: "user1@servihub.com", name: "User One", clerk_id: "124" },
+  ],
+});
+
+await db.report.create({
   data: {
     type: "review",
     target_id: 101,
     reason: "Spam content",
-    submitted_by: 2
-  }
-})
+    submitted_by: 2,
+  },
+});
